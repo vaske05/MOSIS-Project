074101685b4c0c891ce9df290a7efee7339cb30f
Nemanja <nemanjaks4@gmail.com>
2019-01-27 15:34:33 +0100

    Service is not disabled when stopService is called

--- app/src/main/java/com/mosisproject/mosisproject/service/TrackingService.java
+++ app/src/main/java/com/mosisproject/mosisproject/service/TrackingService.java
@@ -11,10 +11,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mosisproject.mosisproject.R;
 import com.mosisproject.mosisproject.model.User;
 import com.mosisproject.mosisproject.model.UserLocation;
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.os.IBinder;
@@ -24,10 +29,14 @@ import android.location.Location;
 import android.content.pm.PackageManager;
 import android.app.Service;
 import android.util.Log;
+import android.widget.Toast;
 
 public class TrackingService extends Service {
 
     private DatabaseReference databaseReference;
+    private LocationCallback locationCallback;
+    private LocationRequest request;
+    private FusedLocationProviderClient LocationClient;
 
     private static final String TAG = TrackingService.class.getSimpleName();
 
@@ -39,30 +48,64 @@ public class TrackingService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        //buildNotification();
         requestLocationUpdates();
+        Log.i(TAG, "Service onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LocationClient.removeLocationUpdates(locationCallback);
+        Log.i(TAG, "Service onDestroyed");
+    }
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+// Create the persistent notification//
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.tracking_enabled_notif))
+
+//Make this notification ongoing so it can’t be dismissed by the user//
+
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.tracking_enabled);
+        startForeground(1, builder.build());
     }
 
     protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+
 //Unregister the BroadcastReceiver when the notification is tapped//
+
             unregisterReceiver(stopReceiver);
+
 //Stop the Service//
+
             stopSelf();
         }
     };
 
-//Initiate the request to track the device's location//
+
+
+    //Initiate the request to track the device's location//
 
     private void requestLocationUpdates() {
-        LocationRequest request = new LocationRequest();
+        request = new LocationRequest();
 
 //Specify how often your app should request the device’s location//
-        request.setInterval(10000);
+        request.setInterval(3000);
 
 //Get the most accurate location data available//
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        LocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
@@ -70,7 +113,7 @@ public class TrackingService extends Service {
 //If the app currently has access to the location permission...//
         if (permission == PackageManager.PERMISSION_GRANTED) {
 //...then request location updates//
-            client.requestLocationUpdates(request, new LocationCallback() {
+            LocationClient.requestLocationUpdates(request,locationCallback = new LocationCallback() {
                 @Override
                 public void onLocationResult(LocationResult locationResult) {
 
